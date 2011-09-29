05072984ec6501bc04b1f41f76e36c30febec9d1
uweschmitt <uschmitt@mineway.de>
2011-09-29 13:35:21 +0200

    splited codegenrator to three files

---
+++ pyOpenMS/cython_code/PXDParser.py
@@ -0,0 +1,183 @@
+from Cython.Compiler.CmdLine import parse_command_line
+from Cython.Compiler.Main import create_default_resultobj, CompilationSource
+from Cython.Compiler import Pipeline
+from Cython.Compiler.Scanning import FileSourceDescriptor
+from Cython.Compiler.Nodes import *
+from Cython.Compiler.ExprNodes import *
+
+from Types import *
+
+import re, os
+
+
+def has(node, lines, what):
+    """ look for 'what' in comment according to 'node'. 
+        'lines' are the line of the file """
+    parts = lines[node.pos[1]-1].split("#")
+    return  len(parts)>1 and what in parts[1]
+
+
+
+
+class Enum(object):
+
+    def __init__(self, node, lines):
+        self.name = node.name
+        self.items = []
+        self.line_number = node.pos[1]
+        self.file_name   = node.pos[0].filename
+        self.wrap = has(node, lines, "wrap")
+
+        current_value = 0
+        for item in node.items:
+            if item.value is not None:
+                current_value = item.value.constant_result
+            self.items.append((item.name, current_value))
+            current_value += 1
+
+    def __str__(self):
+        res = "enum %s : " % self.name
+        res += ", ".join("%s: %d" % (i, v) for (i,v) in self.items )
+        return res
+
+
+class CPPClass(object):
+
+    # allows only one instantiation tuple in comments.
+    # if you want more:
+    #    - make factory from __init__
+    #    - return individual classes
+    #    - further todo: individual Python names for distinct instances
+    #      (maybe via comment:  inst PyClassOne=<B,C> 
+
+
+    def __init__(self, node, lines):
+        self.name = node.name
+        self.line_number = node.pos[1]
+        self.file_name   = node.pos[0].filename
+        self.wrap = has(node, lines, "wrap")
+
+        self.instances = dict() # maps template args to instantiation types
+
+        targs = None
+        if node.templates is not None:
+            # parse comment
+            m = re.search("inst\W*=\W*<(.*)>", lines[self.line_number-1])
+            if self.wrap and not m:
+                raise Exception("need inst=<A,..> arg for template class")
+            instargs = m.group(1).split(",")
+            # template nesting not supported:
+            ttargs=None
+            targs = [ Type(t.strip(), False, False, ttargs) for t in instargs ]
+            self.instances = dict( zip(node.templates, targs) )
+
+        self.type_ = Type(self.name, False, False, targs)
+        self.cpp_repr = cpp_repr(self.type_)
+        self.cython_repr = cython_repr(self.type_)
+        self.python_repr = python_repr(self.type_)
+
+        self.methods = []
+
+        for att in node.attributes:
+            if not has(att, lines, "ignore"):
+                self.methods.append(CPPMethod(att, lines, self.instances))
+
+    def __str__(self):
+        rv = ["class %s: " % cpp_repr(self.type_)]
+        rv += ["     "+str(method) for method in self.methods]
+        return "\n".join(rv)
+        
+
+
+def parse_type(base_type, decl, instances): 
+    """ extracts type information from node in parse tree """
+    targs = None
+    if isinstance(base_type, TemplatedTypeNode):
+        targs = []
+        for arg in base_type.positional_args:
+            if isinstance(arg, CComplexBaseTypeNode):
+                decl = arg.declarator
+                is_ptr = isinstance(decl, CPtrDeclaratorNode)
+                is_ref = isinstance(decl, CReferenceDeclaratorNode)
+                name = arg.base_type.name
+                ttype = instances.get(name, Type(name, is_ptr, is_ref, None))
+                targs.append(ttype)
+            elif isinstance(arg, NameNode):
+                name = arg.name
+                targs.append(instances.get(name, Type(name)))
+            else:   
+                raise Exception("%s,%d: can not handle template arg of type %r"\
+                                % (self.file_name, self.line_number, arg))
+             
+        base_type = base_type.base_type_node
+
+    is_ptr = isinstance(decl, CPtrDeclaratorNode)
+    is_ref = isinstance(decl, CReferenceDeclaratorNode)
+    return instances.get(base_type.name, Type(base_type.name, is_ptr, 
+                                              is_ref, targs))
+    
+
+class CPPMethod(object):
+    
+    def __init__(self, node, lines, instances):
+  
+        self.line_number = node.pos[1]
+        self.file_name   = node.pos[0].filename
+
+        decl = node.declarators[0]
+        self.result_type = parse_type(node.base_type, decl, instances)
+        
+        if isinstance(decl.base, CFuncDeclaratorNode):
+           decl = decl.base
+
+        self.name = decl.base.name
+        self.args = []
+        for arg in decl.args:
+            argdecl = arg.declarator
+            if isinstance(argdecl, CReferenceDeclaratorNode) or \
+               isinstance(argdecl, CPtrDeclaratorNode):
+                argname = argdecl.base.name
+            else:
+                argname = argdecl.name
+            self.args.append((argname, 
+                              parse_type(arg.base_type, argdecl, instances)))
+
+    def __str__(self):
+        rv = cpp_repr(self.result_type)
+        rv += " " + self.name
+        argl = [ cpp_repr(type_)+" "+str(name) for name, type_ in self.args ]
+        return rv + "("+ ", ".join(argl) + ")"
+
+
+def parse(path):
+
+    """ reads pxd file and extracts *SINGLE* class """
+
+    options, sources = parse_command_line(["--cplus", path])
+
+    path = os.path.abspath(path)
+    basename = os.path.basename(path)
+    name, ext = os.path.splitext(basename)
+
+    source_desc = FileSourceDescriptor(path, basename)
+    source = CompilationSource(source_desc, name , os.getcwd())
+    result = create_default_resultobj(source, options)
+
+    context = options.create_context()
+    pipeline = Pipeline.create_pyx_pipeline(context, options, result)
+    tree = pipeline[0](source) # only parser
+
+    if hasattr(tree.body, "stats"):
+        for s in tree.body.stats:
+            if isinstance(s, CDefExternNode):
+                body = s.body
+                break
+    else:
+        body = tree.body.body
+
+    lines = open(path).readlines()
+
+    if isinstance(body, CEnumDefNode):
+            return Enum(body, lines)
+
+    return CPPClass(body, lines)
---
+++ pyOpenMS/cython_code/Types.py
@@ -0,0 +1,46 @@
+class Type(object):
+
+    CTYPES=["int", "long", "double", "float", "char", "void"]
+    LIBCPPTYPES = ["vector", "string", "list" ]
+
+    def __init__(self, basetype, is_ptr=False, is_ref=False, 
+                       template_args=None):
+        self.basetype = "void" if basetype is None else basetype
+        self.is_ptr = is_ptr
+        self.is_ref = is_ref
+        self.template_args = template_args
+
+def cython_repr(type_):
+    """ returns cython type representation """
+
+    if type_.basetype in Type.CTYPES or type_.basetype in Type.LIBCPPTYPES:
+       rv = type_.basetype
+    else:
+        rv = "_"+type_.basetype
+    if type_.template_args is not None:
+        rv += "[%s]" %  ",".join(cython_repr(t) for t in type_.template_args)
+
+    if type_.is_ptr:
+        rv +=" * "
+    elif type_.is_ref:
+        rv +=" & "
+    return rv
+
+def cpp_repr(type_):
+    """ returns C++ type representation """
+
+    rv = type_.basetype
+    if type_.template_args is not None:
+        rv += "<%s>" %  ",".join(cpp_repr(t) for t in type_.template_args)
+
+    if type_.is_ptr:
+        rv +=" * "
+    elif type_.is_ref:
+        rv +=" & "
+    return rv
+
+def python_repr(type_):
+    """ returns Python representation, that is the name the module
+        will expose to its users """
+    return type_.basetype
+
