1ce9dd26a9f151f9f3c7e915bbf44f770bfc9219
uweschmitt <uschmitt@mineway.de>
2011-10-27 12:50:31 +0200

    partially wrapped map alignment classes:
       -  MapAlignmentAlgorithmPoseClustering
       -  TransformationDescription

--- pyOpenMS/cython_code/pxd/MapAlignmentAlgorithmPoseClustering.pxd
+++ pyOpenMS/cython_code/pxd/MapAlignmentAlgorithmPoseClustering.pxd
@@ -4,17 +4,23 @@ from TransformationDescription cimport *
 from MSExperiment cimport *
 from String cimport *
 from Param cimport *
+from ProgressLogger_LogType cimport *
 
 cdef extern from "<OpenMS/ANALYSIS/MAPMATCHING/MapAlignmentAlgorithmPoseClustering.h>" namespace "OpenMS":
     
     #ctypedef MSE MSExperiment[Peak1D, ChromatogramPeak]
     cdef cppclass MapAlignmentAlgorithmPoseClustering: #wrap=True
         MapAlignmentAlgorithmPoseClustering()
-        #void alignPeakMaps(vector[MSExperiment], vector[TransformationDescription] &)
+        void alignPeakMaps(vector[MSExperiment[Peak1D, ChromatogramPeak]], vector[TransformationDescription] &)
     
-        #void setReference(int idx, String file)
-        #void getDefaultModel(String model_type, Param p)
-        #void fitModel(String model_type, Param p, vector[TransformationDescription])
+        void setReference(int idx, String file)
+        void getDefaultModel(String model_type, Param p)
+        void fitModel(String model_type, Param p, vector[TransformationDescription])
         void transformPeakMaps(vector[MSExperiment[Peak1D, ChromatogramPeak]] &, vector[TransformationDescription]) 
         
+        void setLogType(LogType type)
+
+        Param getDefaults()
+        Param getParameters()
+        void setParameters(Param)
         
---
+++ unittests/testMapAlignment.py
@@ -0,0 +1,66 @@
+from pyOpenMS import *
+import numpy as np
+
+def testMapAlignment():
+    """
+    @tests
+    MapAlignmentAlgorithmPoseClustering.__init__
+    .getDefaultModel
+    .alignPeakMaps
+    .fitModel
+    .transformPeakMaps
+    .setReference
+    .setLogType
+
+    String.__init__
+    .c_str
+    Param.__init__
+    .getKeys
+    .getValue
+    """
+    
+    ma = MapAlignmentAlgorithmPoseClustering()
+    s = String()
+    p = Param()
+    ma.getDefaultModel(s, p)
+    ma.setLogType(LogType.CMD)
+
+    # check default settings
+    assert s.c_str() == "linear"
+    assert p.getKeys() == ["symmetric_regression"]
+    assert p.getValue(String("symmetric_regression")).toString() == "true"
+
+    mse = MSExperiment()
+    FileHandler().loadExperiment("test.mzML", mse)
+
+    transformations = []
+    maps = [mse,mse]
+    ma.setReference(2, String())
+    ma.alignPeakMaps(maps, transformations)
+
+    assert len(transformations) == 2
+
+    ps = transformations[0].getDataPoints()
+    assert type(ps) == list
+    assert type(ps[0]) == tuple
+    
+    #print ma.getDefaults().getKeys()
+    #print len(ps), mse.size()
+    #assert len(ps) == mse.size()
+
+    ma.fitModel(s, p, transformations)
+
+    pvorher0 = mse[0].get_peaks()[:10,:]
+    pvorher1 = mse[1].get_peaks()[:10,:]
+
+    ma.transformPeakMaps(maps,  transformations)
+
+    pnachher0 = maps[0][0].get_peaks()[:10,:]
+    pnachher1 = maps[0][1].get_peaks()[:10,:]
+
+    assert np.all(pvorher0==pnachher0)
+    assert np.all(pvorher1==pnachher1)
+
+
+
+
